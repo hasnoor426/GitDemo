fda06fe8dee399c990fd17890b85c7214f1d9c9e
Hasnoor <hasnoor426@gmail.com>
2020-08-19 21:45:24 -0400

    first commit

---
+++ src/main/java/pageObject/LandingPage.java
@@ -0,0 +1,37 @@
+package pageObject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LandingPage {
+	
+	public WebDriver driver;
+	
+	public LandingPage(WebDriver driver) 
+	{		
+		this.driver=driver;
+	}	
+		
+	By signin=By.cssSelector("a[href*='sign_in']"); 
+	public WebElement getLogin()
+	{
+		return driver.findElement(signin);
+	}
+	
+	By title=By.cssSelector(".text-center>h2");
+	public WebElement getTitle()
+	{
+		return driver.findElement(title);
+	}
+	
+	By navBar= By.cssSelector(".nav.navbar-nav.navbar-right>li>a");
+	public WebElement getNavigationBar()
+	{
+		return driver.findElement(navBar); 
+	}
+	
+	//div[class*='video-banner']h3  banner
+	
+
+}
